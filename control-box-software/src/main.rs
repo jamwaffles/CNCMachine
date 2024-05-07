db496cbe4e9918dbf86480b512d25a92c2264975
James Waples <james@wapl.es>
2024-05-07 10:57:45 +0100

    Integrate into LinuxCNC

--- control-box-software/src/main.rs
+++ control-box-software/src/main.rs
@@ -1,4 +1,5 @@
 use control_box_common::Outputs;
+use futures_lite::StreamExt;
 use linuxcnc_hal::{
     error::PinRegisterError,
     hal_pin::{InputPin, OutputPin},
@@ -6,62 +7,105 @@ use linuxcnc_hal::{
     HalComponent, RegisterResources, Resources,
 };
 use postcard::accumulator::{CobsAccumulator, FeedResult};
-use std::io;
-use std::time::Duration;
+use smol::prelude::*;
+use std::os::fd::AsRawFd;
+use std::{error::Error, io};
+use std::{
+    os::fd::{FromRawFd, OwnedFd},
+    time::Duration,
+};
 
 const BUF_SIZE: usize = 1024;
 
 struct Pins {
-    //
+    encoder1: InputPin<u32>,
 }
 
-fn main() {
-    let port_name = "/dev/ttyACM2";
+impl Resources for Pins {
+    type RegisterError = PinRegisterError;
+
+    fn register_resources(comp: &RegisterResources) -> Result<Self, Self::RegisterError> {
+        Ok(Pins {
+            encoder1: comp.register_pin::<InputPin<u32>>("encoder-1")?,
+        })
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    rtapi_logger::init().ok();
+
+    let port_name = std::env::args()
+        .nth(1)
+        .expect("Port name/path required, e.g. /dev/ttyACM0");
     let baud_rate = 115200;
 
-    let port = serialport::new(port_name, baud_rate)
+    let comp: HalComponent<Pins> = HalComponent::new("control-box")?;
+
+    let pins = comp.resources();
+
+    let mut port = serialport::new(port_name.clone(), baud_rate)
         .timeout(Duration::from_millis(10))
-        .open();
+        .open()
+        .expect("Failed to open port");
 
-    match port {
-        Ok(mut port) => {
-            println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
+    println!("Receiving data on {} at {} baud:", &port_name, &baud_rate);
 
-            let mut cobs_buf: CobsAccumulator<BUF_SIZE> = CobsAccumulator::new();
-            let mut raw_buf = [0u8; BUF_SIZE];
+    // let async_port =
+    //     smol::Async::new_nonblocking(unsafe { OwnedFd::from_raw_fd(port.as_raw_fd()) })?;
 
-            loop {
-                let buf = match port.read(&mut raw_buf) {
-                    Ok(ct) => &raw_buf[..ct],
-                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => continue,
-                    Err(ref e) if e.kind() == io::ErrorKind::BrokenPipe => {
-                        panic!("Broken pipe")
-                    }
-                    Err(e) => {
-                        eprintln!("{}", e);
-                        continue;
+    let mut cobs_buf: CobsAccumulator<BUF_SIZE> = CobsAccumulator::new();
+    let mut raw_buf = [0u8; BUF_SIZE];
+
+    smol::block_on(async {
+        // while let Ok(n) = async_port
+        //     .read_with(|t| rustix::io::read(t, &mut raw_buf).map_err(io::Error::from))
+        //     .await
+        // {
+        //     let mut window = &raw_buf[0..n];
+
+        //     'cobs: while !window.is_empty() {
+        //         window = match cobs_buf.feed::<Outputs>(&window) {
+        //             FeedResult::Consumed => break 'cobs,
+        //             FeedResult::OverFull(new_wind) => new_wind,
+        //             FeedResult::DeserError(new_wind) => new_wind,
+        //             FeedResult::Success { data, remaining } => {
+        //                 println!("{:?}", data);
+
+        //                 remaining
+        //             }
+        //         };
+        //     }
+        // }
+
+        while !comp.should_exit() {
+            let buf = match port.read(&mut raw_buf) {
+                Ok(ct) => &raw_buf[..ct],
+                Err(ref e) if e.kind() == io::ErrorKind::TimedOut => continue,
+                Err(ref e) if e.kind() == io::ErrorKind::BrokenPipe => {
+                    panic!("Broken pipe")
+                }
+                Err(e) => {
+                    eprintln!("{}", e);
+                    continue;
+                }
+            };
+
+            let mut window = &buf[..];
+
+            'cobs: while !window.is_empty() {
+                window = match cobs_buf.feed::<Outputs>(&window) {
+                    FeedResult::Consumed => break 'cobs,
+                    FeedResult::OverFull(new_wind) => new_wind,
+                    FeedResult::DeserError(new_wind) => new_wind,
+                    FeedResult::Success { data, remaining } => {
+                        println!("{:?}", data);
+
+                        remaining
                     }
                 };
-
-                let mut window = &buf[..];
-
-                'cobs: while !window.is_empty() {
-                    window = match cobs_buf.feed::<Outputs>(&window) {
-                        FeedResult::Consumed => break 'cobs,
-                        FeedResult::OverFull(new_wind) => new_wind,
-                        FeedResult::DeserError(new_wind) => new_wind,
-                        FeedResult::Success { data, remaining } => {
-                            println!("{:?}", data);
-
-                            remaining
-                        }
-                    };
-                }
             }
         }
-        Err(e) => {
-            eprintln!("Failed to open \"{}\". Error: {}", port_name, e);
-            ::std::process::exit(1);
-        }
-    }
+    });
+
+    Ok(())
 }
