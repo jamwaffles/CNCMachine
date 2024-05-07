f21c2cb6c867c297e03d2a69224e120e6a12c942
James Waples <james@wapl.es>
2024-05-07 14:56:55 +0100

    Rapid override and reset

--- control-box-software/src/main.rs
+++ control-box-software/src/main.rs
@@ -13,9 +13,12 @@ const BUF_SIZE: usize = 1024;
 
 struct Pins {
     feed_override_scale: OutputPin<f64>,
+    rapid_override_scale: OutputPin<f64>,
     feed_override_value: InputPin<f64>,
     encoder1: OutputPin<i32>,
     encoder1_button: OutputPin<bool>,
+    encoder2: OutputPin<i32>,
+    encoder2_button: OutputPin<bool>,
 }
 
 impl Resources for Pins {
@@ -24,9 +27,12 @@ impl Resources for Pins {
     fn register_resources(comp: &RegisterResources) -> Result<Self, Self::RegisterError> {
         Ok(Pins {
             feed_override_scale: comp.register_pin::<OutputPin<f64>>("feed-override-scale")?,
+            rapid_override_scale: comp.register_pin::<OutputPin<f64>>("rapid-override-scale")?,
             feed_override_value: comp.register_pin::<InputPin<f64>>("feed-override-value")?,
             encoder1: comp.register_pin::<OutputPin<i32>>("encoder-1")?,
             encoder1_button: comp.register_pin::<OutputPin<bool>>("encoder-1-button")?,
+            encoder2: comp.register_pin::<OutputPin<i32>>("encoder-2")?,
+            encoder2_button: comp.register_pin::<OutputPin<bool>>("encoder-2-button")?,
         })
     }
 }
@@ -66,9 +72,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut encoder1_prev = None;
     let mut encoder1_value = 0i32;
+    let mut encoder2_prev = None;
+    let mut encoder2_value = 0i32;
 
     // 5% for every detent
     pins.feed_override_scale.set_value(0.05)?;
+    pins.rapid_override_scale.set_value(0.05)?;
 
     while !comp.should_exit() {
         let buf = match port.read(&mut raw_buf) {
@@ -91,6 +100,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 FeedResult::OverFull(new_wind) => new_wind,
                 FeedResult::DeserError(new_wind) => new_wind,
                 FeedResult::Success { data, remaining } => {
+                    // Feed override slider
                     {
                         let prev = *encoder1_prev.get_or_insert(data.encoder1);
                         let new = data.encoder1;
@@ -113,8 +123,32 @@ fn main() -> Result<(), Box<dyn Error>> {
                         encoder1_prev = Some(new);
                     }
 
+                    // Rapid override slider
                     {
-                        pins.encoder1_button.set_value(data.encoder_button1)?;
+                        let prev = *encoder2_prev.get_or_insert(data.encoder2);
+                        let new = data.encoder2;
+
+                        let diff = i32::from(new) - i32::from(prev);
+
+                        let wrapped = diff.abs() > i32::from(u16::MAX / 2);
+
+                        let res = if wrapped {
+                            diff - i32::from(u16::MAX) * diff.signum()
+                        } else {
+                            diff
+                        };
+
+                        encoder2_value += res;
+
+                        // Divide by 4 because quadrature encoder
+                        pins.encoder2.set_value(encoder2_value / 4)?;
+
+                        encoder2_prev = Some(new);
+                    }
+
+                    // Rapid override reset
+                    {
+                        pins.encoder2_button.set_value(data.encoder_button2)?;
                     }
 
                     remaining
